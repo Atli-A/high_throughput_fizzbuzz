510e1678329bb0029acbb9336fda4ce3fbf4ddc4
Atli Arnarsson <atli.nikulas@gmail.com>
2025-07-09 00:28:22 -0400

    progress on crazy idea

--- bcd.zig
+++ bcd.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const dprint = std.debug.print;
+
+/// if implementation fails
+/// https://stackoverflow.com/questions/55802309/is-there-a-256-bit-integer-type-in-c
+
 /// Golden Sequence:
 ///
 /// Consider one fizzbuzz segment which matches the following 
@@ -14,7 +19,7 @@ const std = @import("std");
 /// 
 /// since the ascii digits 0 to 9 are in binary as ?011 XXXX
 /// so '0' is naturally stored as 0011 0000
-/// however we will store it as 1111 0111.
+/// however we will store it as 1111 0111. (adding 7 to the original)
 /// This allows for adding to overflow to the next byte.
 /// All non number bytes are stored as 1111 1111
 ///
@@ -27,6 +32,15 @@ const std = @import("std");
 /// '14 '   1111_1000 1111_1011 1111_1111 
 /// 'FizzBuzz ' 1111_1111 1111_1111 1111_1111 1111_1111 1111_1111 1111_1111 1111_1111 1111_1111 1111_1111 
 ///
+///
+/// to convert these stored numbers to their string version we must perform 
+/// several operations 
+///
+/// what we do for letters does not matter
+/// but for each byte we do the following
+/// 1111_XXXX -> 0011_(XXXX - 7)
+/// 
+/// 1 = 1111_1000 => 0011_0001 = '1'
 
 
 // fizzbuzz tokens are seperated by a seperator
@@ -60,38 +74,122 @@ fn TokensToSequenceLen(sequence: []FizzBuzzToken, num_len: usize, fizz_len: usiz
 }
 
 
+fn IntegerFromSliceWidth(slice: []const u8) type {
+    return @Type(
+        std.builtin.Type.Int{
+            .signedness=std.builtin.Signedness.unsigned,
+            .bits=8*slice.len,
+        }
+    );
+}
 
-fn GoldenSegment(comptime num_len: usize) type {
+
+fn GoldenSegment(comptime num_len: comptime_int) type {
+
     const FIZZ: []const u8 = "Fizz";
     const BUZZ: []const u8 = "Buzz";
     const FIZZBUZZ: []const u8 = "FizzBuzz";
-    const SEPERATOR: []const u8 = "\n";
-    const ZERO: []const u8 = ([1]u8{0})[0..];
+    const SEPERATOR: []const u8 = "|";
+    const NULLCHAR: []const u8 = &[1]u8{0};
+    const ZERO: []const u8 = NULLCHAR**num_len;
+    const EMPTY: []const u8 = "";
 
-    comptime var textLayer: []u8 = &[0]u8{};
+
+
+    comptime var textLayer = EMPTY;
     inline for (StandardFizzBuzz) |token| {
-        
-        const temp: []u8 = switch (token) {
-            FizzBuzzToken.Number => ZERO**num_len,
+        textLayer = textLayer ++ switch (token) {
+            FizzBuzzToken.Number => ZERO,
             FizzBuzzToken.Fizz => FIZZ,
             FizzBuzzToken.Buzz => BUZZ,
             FizzBuzzToken.FizzBuzz => FIZZBUZZ,
         };
-        textLayer = textLayer ++ temp;
         textLayer = textLayer ++ SEPERATOR;
     }
-    @compileLog(textLayer);
 
-
+    const num_bytes = textLayer.len;
+    
+    // this variable is a bitmask for the text 
+    comptime var textMask = EMPTY;
+    inline for (textLayer) |byte| {
+        textMask = textMask ++ &[1]u8{(if (byte == 0x00) 0x00 else 0xFF)};
+    }
 
     return struct {
         const Self = @This();
+        const NumberType = IntegerFromSliceWidth(textLayer);
+        const VectorType = @Vector(u8, num_bytes);
+        const ArrayType = [num_bytes]u8;
+
+        const UnionType = packed union {
+            number: NumberType,
+            vector: VectorType,
+            array: ArrayType,
+        };
+
+        data: UnionType,
+        allocator: std.mem.Allocator,
+
+        fn init(allocator: std.mem.Allocator) Self {
+            dprint("{s}\n", .{textLayer});
+            dprint("{s}\n", .{textMask});
+
+            // TODO align
+            const memory = try allocator.alloc(u8, num_bytes);
+            return .{
+                .allocator = allocator,
+                .data = UnionType{ .vector = @as(VectorType, memory)},
+            };
+        }
+
+        fn initial(self: Self) void {
+            for (0..self.num_bytes) |i| {
+                self.data.array[i] = if (textLayer == 0x00) 0xF7 else 0xFF;
+            }
+            // last one 
+            // self.data.array[self.data.array.len-1] = 1;
+        }
+
+        fn to_str(self: Self, vector: VectorType) VectorType {
+            comptime const safe_version = false;
+            if (safe_version) {
+                return (
+                    (
+                     // isolate lower 4 bits and subtract
+                     ((@splat(0x0F) & vector) - @splat(7))
+                     & @splat(0x0F) // re-isolate to be safe
+                    )
+                    // upper 4 bits
+                    | @splat(0x30) 
+                );
+            } else {
+                // unsafe because i'm not confident it works but its (prolly) faster
+                return (
+                    // remove 7 this is a safe subtraction since we add 
+                    //   seven to the base of any wont affect to the least four
+                    //   significant bits other than subtraction
+                    (vector - @splat(7)) 
+                    // forcibly set the upper bits
+                    | @splat(0x30)
+                );
+            }
+        }
+
+
+        fn build(self: Self) void {
+            // should maybe use select instead @select()
+             const result = (textLayer & textMask) | (self.data.vector & ~textMask);
+        }
+
+        fn free(self: Self) {
+            self.allocator.free(self.data.vector);
+        }
     };
 }
 
 
 
 pub fn main() !void {
-    const x: GoldenSegment(1) = undefined;
-    _ = x;
+    const x = GoldenSegment(3).init(std.heap.page_allocator);
+    x.build();
 }
