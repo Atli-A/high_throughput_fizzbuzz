6de053c57fe868609bf75ca6b166ab0dd94b03d0
Atli Arnarsson <atli.nikulas@gmail.com>
2025-07-09 01:32:04 -0400

    close to working

--- bcd.zig
+++ bcd.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const dprint = std.debug.print;
+const assert = std.debug.assert;
 
 /// if implementation fails
 /// https://stackoverflow.com/questions/55802309/is-there-a-256-bit-integer-type-in-c
@@ -74,11 +75,13 @@ fn TokensToSequenceLen(sequence: []FizzBuzzToken, num_len: usize, fizz_len: usiz
 }
 
 
-fn IntegerFromSliceWidth(slice: []const u8) type {
+fn createInt(bits: comptime_int) type {
     return @Type(
-        std.builtin.Type.Int{
-            .signedness=std.builtin.Signedness.unsigned,
-            .bits=8*slice.len,
+        std.builtin.Type{
+            .@"int"=std.builtin.Type.Int{
+                .signedness=std.builtin.Signedness.unsigned,
+                .bits=bits,
+            }
         }
     );
 }
@@ -90,98 +93,121 @@ fn GoldenSegment(comptime num_len: comptime_int) type {
     const BUZZ: []const u8 = "Buzz";
     const FIZZBUZZ: []const u8 = "FizzBuzz";
     const SEPERATOR: []const u8 = "|";
-    const NULLCHAR: []const u8 = &[1]u8{0};
+    const NULLCHAR: []const u8 = &[1]u8{0x00};
     const ZERO: []const u8 = NULLCHAR**num_len;
     const EMPTY: []const u8 = "";
 
 
 
-    comptime var textLayer = EMPTY;
+    comptime var textLayerSlice = EMPTY;
     inline for (StandardFizzBuzz) |token| {
-        textLayer = textLayer ++ switch (token) {
+        textLayerSlice = textLayerSlice ++ switch (token) {
             FizzBuzzToken.Number => ZERO,
             FizzBuzzToken.Fizz => FIZZ,
             FizzBuzzToken.Buzz => BUZZ,
             FizzBuzzToken.FizzBuzz => FIZZBUZZ,
         };
-        textLayer = textLayer ++ SEPERATOR;
+        textLayerSlice = textLayerSlice ++ SEPERATOR;
     }
 
-    const num_bytes = textLayer.len;
+    const num_bytes = textLayerSlice.len;
+
+    const NumberType = createInt(num_bytes);
+    const VectorType = @Vector(num_bytes, u8);
+    const VectorBoolType = @Vector(num_bytes, bool);
+//    const ArrayType = [num_bytes]u8;
+
+    const textLayer: VectorType = textLayerSlice[0..].*;
+
+    // true when byte is text not number
+    const textMask: VectorBoolType = textLayer != @as(VectorType, @splat(0x00));
+
+    // the default starting number and stuff
+    const number_default = @as(VectorType, @splat(0b1111_0111));
+    const text_default = @as(VectorType, @splat(0xFF));
+    const default_value = @select(u8, textMask, text_default, number_default);
     
-    // this variable is a bitmask for the text 
-    comptime var textMask = EMPTY;
-    inline for (textLayer) |byte| {
-        textMask = textMask ++ &[1]u8{(if (byte == 0x00) 0x00 else 0xFF)};
-    }
+    // this allows us to |= the internal representation with reset_mask to make it ready to be added to again
+    const reset_mask = @select(u8, textMask, @as(VectorType, @splat(0xFF)), @as(VectorType, @splat(0xF0)));
 
     return struct {
         const Self = @This();
-        const NumberType = IntegerFromSliceWidth(textLayer);
-        const VectorType = @Vector(u8, num_bytes);
-        const ArrayType = [num_bytes]u8;
 
         const UnionType = packed union {
             number: NumberType,
             vector: VectorType,
-            array: ArrayType,
+//            array: ArrayType,
         };
+        const len_bytes = num_bytes;
 
         data: UnionType,
         allocator: std.mem.Allocator,
 
-        fn init(allocator: std.mem.Allocator) Self {
-            dprint("{s}\n", .{textLayer});
-            dprint("{s}\n", .{textMask});
+        fn init(allocator: std.mem.Allocator) !Self {
+//            dprint("{s}\n", .{textLayer});
+//            dprint("{s}\n", .{textMask});
 
             // TODO align
             const memory = try allocator.alloc(u8, num_bytes);
+            // so the compiler knows its aligned
+            const vector: VectorType = memory[0..num_bytes].*;
             return .{
                 .allocator = allocator,
-                .data = UnionType{ .vector = @as(VectorType, memory)},
+                .data = UnionType{ .vector = vector},
             };
         }
 
-        fn initial(self: Self) void {
-            for (0..self.num_bytes) |i| {
-                self.data.array[i] = if (textLayer == 0x00) 0xF7 else 0xFF;
-            }
-            // last one 
-            // self.data.array[self.data.array.len-1] = 1;
+        /// initializes to the zero value for our internal number
+        fn initial(self: *Self) void {
+            self.data.vector = default_value;
         }
 
-        fn to_str(self: Self, vector: VectorType) VectorType {
-            comptime const safe_version = false;
+        fn to_str(self: Self) VectorType {
+            const safe_version = false;
+            var reformed_numbers: VectorType = undefined;
             if (safe_version) {
-                return (
+                reformed_numbers = (
                     (
                      // isolate lower 4 bits and subtract
-                     ((@splat(0x0F) & vector) - @splat(7))
-                     & @splat(0x0F) // re-isolate to be safe
+                     (@as(VectorType, @splat(0x0F)) & self.data.vector) - @as(VectorType, @splat(7))
+                     & @as(VectorType, @splat(0x0F)) // re-isolate to be safe
                     )
                     // upper 4 bits
-                    | @splat(0x30) 
+                    | @as(VectorType, @splat(0x30))
                 );
             } else {
                 // unsafe because i'm not confident it works but its (prolly) faster
-                return (
+                reformed_numbers = (
                     // remove 7 this is a safe subtraction since we add 
                     //   seven to the base of any wont affect to the least four
                     //   significant bits other than subtraction
-                    (vector - @splat(7)) 
+                    (self.data.vector - @as(VectorType, @splat(7)))
                     // forcibly set the upper bits
-                    | @splat(0x30)
+                    | @as(VectorType, @splat(0x30))
                 );
             }
+            // mix in words
+            return @select(u8, textMask, textLayer, reformed_numbers);
         }
 
 
-        fn build(self: Self) void {
-            // should maybe use select instead @select()
-             const result = (textLayer & textMask) | (self.data.vector & ~textMask);
+        /// destructive
+        fn add(self: Self, adding: u64) void {
+
+            // add to the number 
+            self.data.number += @intCast(adding);
+            // restore rules so this stuff can be used again
+            self.data.vector |= reset_mask;
         }
 
-        fn free(self: Self) {
+        fn verify_internal_representation(self: Self) void {
+            // TODO @reduce over internal vector and verify that lower 4 bits always >= 7
+            // assert();
+            _ = self;
+        }
+        
+        /// destructive
+        fn free(self: Self) void {
             self.allocator.free(self.data.vector);
         }
     };
@@ -190,6 +216,10 @@ fn GoldenSegment(comptime num_len: comptime_int) type {
 
 
 pub fn main() !void {
-    const x = GoldenSegment(3).init(std.heap.page_allocator);
-    x.build();
+    const T = GoldenSegment(3);
+    var x = try T.init(std.heap.page_allocator);
+    x.initial();
+    const asdf: [T.len_bytes]u8 = x.to_str();
+    dprint("{s}\n", .{asdf});
+
 }
