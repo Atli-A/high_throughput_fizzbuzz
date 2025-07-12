17b5f74eb0661d34c83ebfd792cb1ead84529645
Atli Arnarsson <atli.nikulas@gmail.com>
2025-07-12 18:27:41 -0400

    new progress

--- bcd.zig
+++ bcd.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const dprint = std.debug.print;
 const assert = std.debug.assert;
@@ -44,6 +45,108 @@ const assert = std.debug.assert;
 /// 1 = 1111_1000 => 0011_0001 = '1'
 
 
+
+
+
+fn createInt(bits: comptime_int) type {
+    return @Type(
+        std.builtin.Type{
+            .@"int"=std.builtin.Type.Int{
+                .signedness=std.builtin.Signedness.unsigned,
+                .bits=bits,
+            }
+        }
+    );
+}
+
+
+fn StrInt(comptime _len: comptime_int) type {
+    return struct {
+        const Self = @This();
+        const length = _len;
+
+        const IntType = createInt(length*8);
+        const StrType = @Vector(length, u8);
+        const ArrType = u8[length];
+
+        const UnionType = packed union {
+            str: StrType,
+            int: IntType,
+        };
+
+        number: UnionType,
+        const ZERO_VALUE: u8 = 0xF6;
+        const MIN_INTERNAL: u8 = ZERO_VALUE & 0x0F;
+
+
+        fn init() Self {
+            if (@bitSizeOf(IntType) != @bitSizeOf(UnionType) or @bitSizeOf(StrType) != @bitSizeOf(UnionType)) {
+                @compileError(
+                    std.fmt.comptimePrint("mismatch sizes", .{})
+                );
+           }
+            return .{
+                .number = UnionType{.int = 0},
+            };
+        }
+
+        fn splat(x: u8) StrType {
+            return @splat(x);
+        }
+
+        /// Zeroes the entire UnionType 
+        fn zero(self: *Self) void {
+            self.number.str = splat(ZERO_VALUE);
+        }
+
+        fn debug(self: Self) void {
+            for (0..length) |i| {
+                dprint("{0X:0<2.2} ", .{self.number.str[i]});
+            }
+            dprint("\n", .{});
+        }
+
+        fn invariant(self: Self) void {
+            assert(
+                @reduce(.And, ((self.number.str & splat(0x0F)) >= splat(MIN_INTERNAL)))
+            );
+        }
+
+        fn add(self: *Self, x: comptime_int) void {
+            // add
+            self.number.int +%= x;
+
+            // enforce internal representation
+
+            // where we need to add 7
+            const need_correction = (splat(0x0F) & self.number.str) < splat(MIN_INTERNAL);
+            // currect when we need to add 7
+            self.number.str +%= @select(u8, need_correction, splat(MIN_INTERNAL), splat(0x00));
+            // set upper 4 bits to 1
+            self.number.str |= splat(0xF0);
+            self.invariant();
+        }
+
+        fn to_str(self: *Self, out: *ArrType) void {
+            const endianness = builtin.target.cpu.arch.endian();
+            comptime if (endianness == .little) {
+                self.number.int = @byteSwap(self.number.int);
+            }
+
+            out.* = (
+                // the lower 4 bits are just the number itself not its internal representation
+                (self.number.str & splat(0x0F)) - splat(MIN_INTERNAL)
+                // upper bits are just 0x30
+                | splat(0x30) 
+            );
+
+            comptime if (endianness == .little) {
+                self.number.int = @byteSwap(self.number.int);
+            }
+        }
+    };
+}
+
 // fizzbuzz tokens are seperated by a seperator
 const FizzBuzzToken = enum {
     Number, 
@@ -60,209 +163,72 @@ const StandardFizzBuzz = [_]FizzBuzzToken{
     FizzBuzzToken.Number, FizzBuzzToken.Number, FizzBuzzToken.FizzBuzz,
 };
 
-fn TokensToSequenceLen(sequence: []FizzBuzzToken, num_len: usize, fizz_len: usize, buzz_len: usize, fizzbuzz_len: usize, sep_len: usize) usize {
-    const total: usize = 0;
-    for (sequence) |token| {
-        total += switch (token) {
-            FizzBuzzToken.Number => num_len,
-            FizzBuzzToken.Fizz => fizz_len,
-            FizzBuzzToken.Buzz => buzz_len,
-            FizzBuzzToken.FizzBuzz => fizzbuzz_len,
-        };
-        total += sep_len;
-    }
-    return total;
-}
-
-
-fn createInt(bits: comptime_int) type {
-    return @Type(
-        std.builtin.Type{
-            .@"int"=std.builtin.Type.Int{
-                .signedness=std.builtin.Signedness.unsigned,
-                .bits=bits,
-            }
-        }
-    );
-}
-
-
-fn GoldenSegment(comptime num_len: comptime_int) type {
-
-    const FIZZ: []const u8 = "Fizz";
-    const BUZZ: []const u8 = "Buzz";
-    const FIZZBUZZ: []const u8 = "FizzBuzz";
-    const SEPERATOR: []const u8 = "|";
-    const NULLCHAR: []const u8 = &[1]u8{0x00};
-    const ZERO: []const u8 = NULLCHAR**num_len;
-    const EMPTY: []const u8 = "";
 
 
 
-    comptime var textLayerSlice = EMPTY;
-    inline for (StandardFizzBuzz) |token| {
-        textLayerSlice = textLayerSlice ++ switch (token) {
-            FizzBuzzToken.Number => ZERO,
-            FizzBuzzToken.Fizz => FIZZ,
-            FizzBuzzToken.Buzz => BUZZ,
-            FizzBuzzToken.FizzBuzz => FIZZBUZZ,
-        };
-        textLayerSlice = textLayerSlice ++ SEPERATOR;
-    }
 
-    const num_bytes = textLayerSlice.len;
+/// a FizzBuzzer is in charge of printing 
+fn FizzBuzzer(
+    comptime number_length: comptime_int,
+    ) type {
 
-    const NumberType = createInt(num_bytes*8);
-//    const NumberStrInt = createInt(num_len*8);
-    const VectorType = @Vector(num_bytes, u8);
-    const VectorBoolType = @Vector(num_bytes, bool);
-//    const ArrayType = [num_bytes]u8;
-
-    const textLayer: VectorType = textLayerSlice[0..].*;
-
-    // true when byte is text not number
-    const textMask: VectorBoolType = textLayer != @as(VectorType, @splat(0x00));
-
-    // the default starting number and stuff
-    const number_default = @as(VectorType, @splat(0b1111_0110));
-    const text_default = @as(VectorType, @splat(0xFF));
-    const default_value = @select(u8, textMask, text_default, number_default);
+    // TODO this usize will be a problem
+    
+    const Sequence: []FizzBuzzToken = StandardFizzBuzz;
+    const Fizz: u8[] = "Fizz";
+    const Buzz: u8[] = "Buzz";
+    const FizzBuzz: u8[] = "FizzBuzz";
+    const Seperator: u8[] = "|"; // should be newline
     
 
-    return struct {
-        const Self = @This();
 
-        const UnionType = packed union {
-            number: NumberType,
-            vector: VectorType,
-//            array: ArrayType,
-        };
-        const len_bytes = num_bytes;
-
-        data: UnionType,
-        allocator: std.mem.Allocator,
-
-        fn init(allocator: std.mem.Allocator) !Self {
-//            dprint("{s}\n", .{textLayer});
-//            dprint("{s}\n", .{textMask});
-
-            // TODO align
-            const memory = try allocator.alloc(u8, num_bytes);
-            // so the compiler knows its aligned
-            const vector: VectorType = memory[0..num_bytes].*;
-            return .{
-                .allocator = allocator,
-                .data = UnionType{ .vector = vector},
+    /// currently comptime only
+    fn buildSegmentTemplate(FizzBuzzSequence: []FizzBuzzToken) []u8 {
+        var result: u8[] = "";
+        const zero_arr: u8[] = {0x00};
+        inline for (Sequence) |Token| {
+            result = result ++ switch (Token) {
+                .Number => zero_arr**number_length,
+                .Fizz => Fizz,
+                .Buzz => Buzz,
+                .FizzBuzz => FizzBuzz,
             };
+            result = result ++ Seperator
         }
+        return result;
+    }
+    
+    const starting_number = try std.math.powi(comptime_int, 10, number_length-1);
+    const segment_start_pos = (starting_number - 1) % Sequence.len;
+    // 9 since we are in base 10
+    const segment_count = (9*starting_number)/Sequence.len;
+    /// the remainder segment is the very last one which (may) not be full length
+    const remainder_segment_length = (9*starting_number) % Sequence.len;
 
-        /// initializes to the zero value for our internal number
-        fn initial(self: *Self) void {
-            self.data.vector = default_value;
-        }
+    const template = buildSegmentTemplate(
+        Sequence[segment_start_pos..Sequence.len] ++ Sequence[0..segment_start_pos]
+    );
+//    const remainder_template = buildSegmentTemplate(
+//        Sequence[segment_start_pos..segment_start_pos+remainder_segment_length]
+//    );
 
-        fn splat(comptime n: comptime_int) VectorType {
-            return @as(VectorType, @splat(n));
-        }
-
-        fn to_str(self: Self) VectorType {
-            const safe_version = true;
-            var reformed_numbers: VectorType = undefined;
-            if (safe_version) {
-                reformed_numbers = (
-                    (
-                     // isolate lower 4 bits and subtract
-                     (splat(0x0F) & self.data.vector) - splat(6)
-                     & splat(0x0F) // re-isolate to be safe
-                    )
-                    // upper 4 bits
-                    | splat(0x30)
-                );
-            } else {
-                // unsafe because i'm not confident it works but its (prolly) faster
-                reformed_numbers = (
-                    // remove 6 this is a safe subtraction since we add 
-                    //   seven to the base of any wont affect to the least four
-                    //   significant bits other than subtraction
-                    (self.data.vector - splat(6))
-                    // forcibly set the upper bits
-                    | splat(0x30)
-                );
-            }
-            // mix in words
-            return @select(u8, textMask, textLayer, reformed_numbers);
-        }
-
-
-        /// destructive
-        fn add(self: *Self, adding: u64) void {
-
-            // add to the number 
-            self.data.number = @byteSwap(@byteSwap(self.data.number) + @as(NumberType, @intCast(adding)));
-
-            // restore rules so this stuff can be used again
-            // this allows us to |= the internal representation with reset_mask to make it ready to be added to again
-            const less_than_seven: VectorBoolType = self.data.vector <= splat(6);
-            const seven_corrected = self.data.vector + @select(u8, less_than_seven, splat(6), splat(0));
-            self.data.vector = @select(u8, 
-                textMask, 
-                splat(0xFF), 
-                seven_corrected | splat(0x30)
-            );
-        }
-
-        fn verify_internal_representation(self: Self) void {
-            // TODO @reduce over internal vector and verify that lower 4 bits always >= 7
-            // assert();
-            _ = self;
-        }
-        
-        /// destructive
-        fn free(self: Self) void {
-            self.allocator.free(self.data.vector);
-        }
-    };
 }
 
 
 
 pub fn main() !void {
-    const T = GoldenSegment(5);
-    var x = try T.init(std.heap.page_allocator);
-    x.initial();
-    var asdf: [T.len_bytes]u8 = x.to_str();
-    asdf = x.to_str();
-    dprint("{s}\n", .{asdf});
-    for (0..10000) |_| {
-        asdf = x.to_str();
+    const stdout = std.io.getStdOut().writer();
+
+    const digits = 5;
+    const T = StrInt(digits);
+    var n = T.init();
+
+    n.zero();
+    n.invariant();
+    var vec: T.ArrType = undefined;
+    for (0..99999) |_| {
+        n.to_str(&vec);
+        try stdout.print("{s}\n", .{vec});
+        n.add(1);
     }
-    dprint("{s}\n", .{asdf});
-
-
 }
-
-
-
-// the current problem and how to fix it 
-// currently realized a problem with how this is done
-// right now we are adding to the whole block like it is one thing.
-// this is close to but not correct
-// however, i think this is saveable
-// first start using the newlines and other text as buffers
-// then add to the first byte of a number but not the whole thing
-// this can be done comptime at the begginning
-//
-//
-//
-//
-// problem endianness when adding:
-// 
-//
-// u4 version:
-// 1111 
-//
-//
-//
-//
-//
-//
